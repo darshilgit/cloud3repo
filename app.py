2a456540c3875b9c9f255525c0f2cf7a167321b3
Darshil Parikh <darshil.parikh@gmail.com>
2019-02-24 15:42:47 -0600

    scommit

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ import redis
 
 app = Flask(__name__)
 
+redis_connect_dict = {}
+redis_connect_dict['host'] = 'cloud3redis.redis.cache.windows.net'
+redis_connect_dict['port'] = 6380
+redis_connect_dict['db'] = 0
+redis_connect_dict['password'] = 'iMB7hsBunvCnYzZPekiBk+ZAX3TzDKzDplY+Pc8Y2+s='
 
+r = redis.StrictRedis()
 @app.route('/')
 def hello_world():
     conn = db.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:cloud3dbserver.database.windows.net,1433;Database=cloud3db;Uid=dbuser@cloud3dbserver;Pwd={Mypassword!};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
@@ -12,11 +18,15 @@ def hello_world():
     cursor = conn.cursor()
     cursor.execute('Select count(*) from quakes')
     for row in cursor.fetchall():
-        print(row)
+        return str(row)
     # if cursor:
     #     print("connected")
-    return 'Hello World from library'
+    #return 'Hello World from library'
 
 
 if __name__ == '_main_':
     app.run()
+
+
+# darshil_parikh@Azure:/usr/local/lib/python2.7/dist-packages$ pip install pyodbc --user
+# az webapp config set --resource-group appsvc_rg_Linux_centralus --name cloud3app --linux-fx-version "PYTHON|3.5"
