d6d1d86625e12e3cb5774b7b65b49b8dd50070c5
Darshil Parikh <darshil.parikh@gmail.com>
2019-02-24 15:52:51 -0600

    tcommit

--- app.py
+++ app.py
@@ -3,18 +3,24 @@ import pyodbc as db
 import redis
 
 app = Flask(__name__)
-
+conn = db.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:cloud3dbserver.database.windows.net,1433;Database=cloud3db;Uid=dbuser@cloud3dbserver;Pwd={Mypassword!};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
 redis_connect_dict = {}
 redis_connect_dict['host'] = 'cloud3redis.redis.cache.windows.net'
 redis_connect_dict['port'] = 6380
 redis_connect_dict['db'] = 0
 redis_connect_dict['password'] = 'iMB7hsBunvCnYzZPekiBk+ZAX3TzDKzDplY+Pc8Y2+s='
 
-r = redis.StrictRedis()
+r = redis.StrictRedis(redis_connect_dict['host'],
+                      redis_connect_dict['port'],
+                      redis_connect_dict['db'],
+                      redis_connect_dict['password'],
+                      ssl=True)
 @app.route('/')
 def hello_world():
-    conn = db.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:cloud3dbserver.database.windows.net,1433;Database=cloud3db;Uid=dbuser@cloud3dbserver;Pwd={Mypassword!};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
-
+    # conn = db.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:cloud3dbserver.database.windows.net,1433;Database=cloud3db;Uid=dbuser@cloud3dbserver;Pwd={Mypassword!};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+    r.set('somekey','value')
+    res = r.get('somekey')
+    return res
     cursor = conn.cursor()
     cursor.execute('Select count(*) from quakes')
     for row in cursor.fetchall():
