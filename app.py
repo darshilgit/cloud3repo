fd750f0271d50824828eb40a7eb66716d99366ab
Darshil Parikh <darshil.parikh@gmail.com>
2019-02-24 23:09:40 -0600

    added redis

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import pyodbc as db
 import redis
+import pygal
+import time
 
 app = Flask(__name__)
 conn = db.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:cloud3dbserver.database.windows.net,1433;Database=cloud3db;Uid=dbuser@cloud3dbserver;Pwd={Mypassword!};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
@@ -15,19 +17,71 @@ r = redis.StrictRedis(redis_connect_dict['host'],
                       redis_connect_dict['db'],
                       redis_connect_dict['password'],
                       ssl=True)
+
+line_chart = pygal.Bar()
+line_chart.add('a', [1, 2])
+line_chart.add('b', [1, 3])
+
+
+def redis_query(query):
+    if r.get(query) == None:
+        cursor = conn.cursor()
+        rcount = cursor.execute(query).fetchall()
+        r.set(query, rcount[0][0])
+        return None
+    else:
+        rcount= r.get(query)
+        return rcount
+#redisconn()
+
+
 @app.route('/')
 def hello_world():
-    # conn = db.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:cloud3dbserver.database.windows.net,1433;Database=cloud3db;Uid=dbuser@cloud3dbserver;Pwd={Mypassword!};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
-    r.set('somekey','value')
-    res = r.get('somekey')
-    return res
-    cursor = conn.cursor()
-    cursor.execute('Select count(*) from quakes')
-    for row in cursor.fetchall():
-        return str(row)
-    # if cursor:
-    #     print("connected")
-    #return 'Hello World from library'
+    # r.set('somekey','value')
+    # res = r.get('somekey')
+    # return res
+    return render_template('common.html')
+
+@app.route('/question1', methods=['GET'])
+def query_db():
+    return render_template('question1.html', )
+
+@app.route('/question1_execute', methods=['GET'])
+def query_db_execute():
+    mag = request.args.get('mag')
+    oper = request.args.get('oper')
+    # if mag != '' and oper != '':
+
+
+    try:
+        # if mag != '' and oper != '':
+        cursor = conn.cursor()
+        # sql = '''SELECT COUNT(*) FROM QUIZ2TABLE where "mag" < ? '''
+        #         # cursor.execute(sql, (mag,))
+        sql = "SELECT COUNT(*) FROM quakes where mag " + oper + mag
+        if request.args.get('form') == 'Execute':
+            startTime = time.perf_counter()
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            endTime = time.perf_counter()
+            result = result[0][0]
+            total_time = endTime-startTime
+        elif request.args.get('form') == 'ExecuteCache':
+            startTime = time.perf_counter()
+            result = redis_query(sql)
+            endTime = time.perf_counter()
+            if result != None:
+                result = str(result)
+                result = result.split('b')
+                result = result[-1]
+            total_time = endTime - startTime
+    except:
+        result = "error try again"
+    # finally:
+    #     conn.close()
+    return render_template('question1.html', result=result, total_time=total_time)
+
+
 
 
 if __name__ == '_main_':
